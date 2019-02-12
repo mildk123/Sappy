0da58888cfd442057f16d19e5fedc3be6104d3c4
Milad Khan <mildk123@gmail.com>
2019-02-12 14:32:28 +0500

    adding node

--- Admin/Routes/admin.js
+++ Admin/Routes/admin.js
@@ -3,26 +3,55 @@ const routes = express.Router();
 
 const Admin = require('../Model/admin')
 
-// ///////////////// Employees List ////////////////////
-routes.get("/login", async (req, res) => {
-    console.log('login admin')
-    //Check Email
-    // const Admin = await employees.find({});
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
-    res.status(200).send({ message: "Sahi Hai123 mere bhai" });
-    res.end()
+// ///////////////// admins List ////////////////////
+routes.post("/register", (req, res) => {
+    console.log('registering admin')
+    const user = req.body;
+    const hash = hashPassword(user.password);
+
+    const newAdmin = new Admin({ admin_Email: user.email, admin_Password: hash });
+    const token = jwt.sign({ user: user[0] }, 'sappy_125');
+
+    newAdmin.save()
+        .then(() => res.json({ message: "Admin registered successfully!", match: true, token: token, }))
+        .catch(e => res.status(500).send({ message: e.message, match: false }));
 })
 
-routes.get("/register", async (req, res) => {
-    console.log('Registeration of admin')
-    //Check Email
-    // const Admin = await employees.find({});
+routes.post("/login", async (req, res) => {
+    console.log('Authenticating Admin');
 
-    res.status(200).send({ message: "Sahi Hai mere bhai"});
-    res.end()
+    //Check Email
+    const currentAdmin = await Admin.find({ admin_Email: req.body.email });
+    
+    if (!currentAdmin.length) {
+        res.status(500).send({ message: "Incorrect Email/Password!", match: false });
+        return;
+    }
+
+    //Compare Email
+    const passwordMatched = await bcrypt.compareSync(req.body.password, currentAdmin[0].admin_Password);
+
+    if (!passwordMatched) {
+        res.status(500).send({ message: "Incorrect Email/Password!", match: false });
+        return;
+    }
+
+    //Generate Token
+    const token = await jwt.sign({ user: currentAdmin[0] }, 'sappy_125');
+
+    res.send({ token: token, match: true });
 })
 
 
+function hashPassword(password) {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    return hash;
+}
 
 
 module.exports = routes;
