6d95cdafe86af14e42ec816e02ac91830642b940
Milad Khan <mildk123@gmail.com>
2019-02-18 04:36:27 +0500

    added setting drawer

--- Client/Screens/Notifications/index.js
+++ Client/Screens/Notifications/index.js
@@ -1,14 +1,90 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
 
-export class Notifications extends Component {
+import { Spinner } from 'native-base';
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import Header from '../../Helper/Header';
+
+import firebase from '../../config'
+const database = firebase.database().ref()
+
+const { height } = Dimensions.get("window");
+
+class Notifications extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoading: true,
+    }
+  }
+
+
+
+  static navigationOptions = {
+    header: null
+  };
+
+
   render() {
+    const { isLoading, Categories } = this.state;
+    if (isLoading) {
+      return (
+        <View style={styles.container}>
+          <Header
+            headerColor="#47bc72"
+            icon={'menu'}
+            title={"Notifications"}
+            hasTabs={false}
+            searchBar={false}
+            favBtn={false}
+            threeDots={false}
+          />
+          <View style={styles.contentDiv}>
+            <Spinner color='green' />
+          </View>
+        </View>
+      )
+    }
+
     return (
-      <View>
-        <Text> Notifications </Text>
+      <View style={styles.container}>
+        <Header
+          headerColor="#47bc72"
+          icon={'menu'}
+          title={"Notifications"}
+          hasTabs={false}
+          searchBar={false}
+          favBtn={false}
+          threeDots={false}
+        />
+        <View style={styles.contentDiv}>
+          <Text>Notifications</Text>
+        </View>
+
       </View>
     )
   }
 }
 
+Notifications.navigationOptions = {
+  drawerLabel: "Notifications",
+  drawerIcon: ({ tintColor }) => (
+    <Icon name="gg" style={{ color: tintColor, fontSize: 25 }} />
+  )
+};
+
 export default Notifications;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  contentDiv: {
+    padding: 20,
+    flexDirection: "column",
+    alignItems: "center"
+  }
+});
--- Client/Screens/Settings/index.js
+++ Client/Screens/Settings/index.js
@@ -1,14 +1,90 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
 
-export class Settings extends Component {
+import { Spinner } from 'native-base';
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import Header from '../../Helper/Header';
+
+import firebase from '../../config'
+const database = firebase.database().ref()
+
+const { height } = Dimensions.get("window");
+
+class Settings extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoading: true,
+    }
+  }
+
+
+
+  static navigationOptions = {
+    header: null
+  };
+
+
   render() {
+    const { isLoading, Categories } = this.state;
+    if (isLoading) {
+      return (
+        <View style={styles.container}>
+          <Header
+            headerColor="#47bc72"
+            icon={'menu'}
+            title={"Settings"}
+            hasTabs={false}
+            searchBar={false}
+            favBtn={false}
+            threeDots={false}
+          />
+          <View style={styles.contentDiv}>
+            <Spinner color='green' />
+          </View>
+        </View>
+      )
+    }
+
     return (
-      <View>
-        <Text> Settings </Text>
+      <View style={styles.container}>
+        <Header
+          headerColor="#47bc72"
+          icon={'menu'}
+          title={"Settings"}
+          hasTabs={false}
+          searchBar={false}
+          favBtn={false}
+          threeDots={false}
+        />
+        <View style={styles.contentDiv}>
+          <Text>Settings</Text>
+        </View>
+
       </View>
     )
   }
 }
 
-export default Settings
+Settings.navigationOptions = {
+  drawerLabel: "Settings",
+  drawerIcon: ({ tintColor }) => (
+    <Icon name="settings" style={{ color: tintColor, fontSize: 25 }} />
+  )
+};
+
+export default Settings;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  contentDiv: {
+    padding: 20,
+    flexDirection: "column",
+    alignItems: "center"
+  }
+});
