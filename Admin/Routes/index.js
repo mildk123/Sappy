95f882af5126ed15b0ea2afd47f0975c5be13057
Milad Khan <mildk123@gmail.com>
2019-02-22 06:49:30 +0500

    removing project

--- Admin/Routes/index.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-
-router.use('/auth', require('./auth'));
-router.use('/admin', require('./admin'));
-
-module.exports = router;
